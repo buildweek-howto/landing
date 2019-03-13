8a9504da5286334d56989bbf343f1f4855013a10
danpatrickoneill <dan47573@gmail.com>
2019-03-13 18:12:13 -0400

    Added header animations and fixed header logo

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+// Mobile menu component
+
 class MobileMenu {
   constructor(element) {
     this.element = element;
@@ -5,7 +7,6 @@ class MobileMenu {
     this.button = document.querySelector(".navigation-hamburger-button-img");
 
     this.links = document.querySelectorAll(".navigation-link-mobile");
-    console.log(this.links);
 
     this.button.addEventListener("click", () => {
       this.toggleMenu();
@@ -18,10 +19,12 @@ class MobileMenu {
 
   toggleMenu() {
     if (!this.element.classList.contains("hidden-mobile-menu")) {
-      TweenMax.set(this.element, { opacity: 0 });
-      TweenMax.to(this.element, 0.5, {
+      TweenLite.set(this.element, { opacity: 0 });
+      TweenLite.to(this.element, 0.5, {
         className: "+= hidden-mobile-menu",
-        opacity: 1
+        opacity: 1,
+        // Need this to keep from hiding in mobile view
+        zIndex: 1
       });
       this.button.src = "./assets/hamburger-menu-icon-close.svg";
     } else {
@@ -31,6 +34,47 @@ class MobileMenu {
   }
 }
 
+// Mobile menu component instantiation
+
 const mobileMenu = new MobileMenu(
   document.querySelector(".navigation-links-mobile")
 );
+
+// Header animations
+const headerText = document.getElementsByClassName("header-description")[0];
+const headerImg = document.getElementsByClassName("header-image")[0];
+
+TweenLite.fromTo(headerText, 1.5, { x: -500 }, { ease: Expo.easeOut, x: 0 });
+
+TweenLite.fromTo(headerImg, 1.5, { x: 500 }, { ease: Expo.easeOut, x: 0 });
+
+// Header animations with individual elements; button does not work
+// const headerText = document.querySelector(".header-description h1");
+// const headerPara = document.querySelector(".header-description p");
+// const headerBtn = document.querySelector(".header-description button");
+
+// const headerImg = document.getElementsByClassName("header-image")[0];
+
+// TweenLite.fromTo(headerText, 2, { x: -500 }, { ease: Expo.easeOut, x: 0 });
+// TweenLite.fromTo(
+//   headerPara,
+//   1.5,
+//   { x: -500 },
+//   { ease: Expo.easeOut, x: 0 }
+// ).delay(0.5);
+// TweenLite.fromTo(headerBtn, 2, { x: -500 }, { ease: Expo.easeOut, x: 0 });
+
+// TweenLite.fromTo(headerImg, 2, { x: 500 }, { ease: Expo.easeOut, x: 0 });
+
+// Working on fixed logo
+const logo = document.getElementsByClassName("navigation-logo")[0];
+const logoFixed = document.getElementsByClassName("navigation-logo-fixed")[0];
+console.log(logo, logoFixed);
+if (window.innerWidth > 768) {
+  window.addEventListener("scroll", function() {
+    logoFixed.style.display = "block";
+    logo.style.visibility = "hidden";
+  });
+}
+
+console.log(window.innerWidth);
